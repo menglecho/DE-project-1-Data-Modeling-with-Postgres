09380d38d6e80b9cbbd167bdb1efc11c11e4914e
Meng Liang A <meng.a.liang@ericsson.com>
2020-06-12 11:22:09 +0200

    Update sql query for the user table

--- sql_queries.py
+++ sql_queries.py
@@ -86,7 +86,7 @@ SET start_time = %s,
 user_table_insert = ("""
 INSERT INTO users(user_id, first_name,last_name, gender,level) 
         VALUES(%s,%s,%s,%s,%s)
-ON CONFLICT DO NOTHING;
+ON CONFLICT (user_id) DO UPDATE SET level=EXCLUDED.level
 """)
 
 song_table_insert = ("""
